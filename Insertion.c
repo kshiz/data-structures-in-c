bd31ddd610067fd5f154a6bb4dda769198a2ec3e
neddstarkk <nedheesh.hasija@gmail.com>
2019-08-20 09:42:08 +0530

    Insertion sort logic fix

--- Insertion.c
+++ Insertion.c
@@ -15,10 +15,10 @@ void main(){
         temp=a[i];
         j=i-1;
         while((temp<a[j])&&(j>=0)){
-            a[j++]=a[j];
-            j--;
+            a[j+1]=a[j];
+            j=j-1;
         }
-        a[j++]=temp;
+        a[j+1]=temp;
     }
     for(i=0;i<n;i++){
         printf("%d\n",a[i]);
