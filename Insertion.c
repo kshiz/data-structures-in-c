59330ccbee75e9a7527718f34f6cbb044f73cb5b
Kshiz <tkshitij815@gmail.com>
2019-08-20 09:25:00 +0530

    insertion sort

---
+++ Insertion.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main(){
+    int a[10];
+    int i;
+    int n;
+    printf("enter the size");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("\nEnter the element");
+        scanf("%d",&a[i]);
+    }
+    int j;
+    int temp;
+    for(i=1;i<n;i++){
+        temp=a[i];
+        j=i-1;
+        while((temp<a[j])&&(j>=0)){
+            a[j++]=a[j];
+            j--;
+        }
+        a[j++]=temp;
+    }
+    for(i=0;i<n;i++){
+        printf("%d\n",a[i]);
+    }
+}
